a9780c50d20935164228f083615dca033f1a2b9c
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-02-22 21:24:13 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,18 +32,8 @@ const Navbar = () => {
                  ))}
              </ul>
            </nav>
-           {/* <a href="https://drive.google.com/file/d/1vuPKlvINHxjV9UVpiAVklwPEP2lhDi3Y/view?usp=sharing"
-              className="contact-btn group flex flex-col" target="_blank">
-               <div className="inner">
-                   <span>My Resume</span>
-               </div>
-           </a>
-           <a href="#contact" className="contact-btn group">
-             <div className="inner">
-               <span>Contact me</span>
-             </div>
-           </a> */}
-             <div className="flex gap-3 md:gap-0">
+        
+             <div className="flex gap-3 md:gap-5">
                   <a
                     href="https://drive.google.com/file/d/1vuPKlvINHxjV9UVpiAVklwPEP2lhDi3Y/view?usp=sharing"
                     className="contact-btn group"
