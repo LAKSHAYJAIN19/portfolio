a3dad0b450173a1daabab0b10b084681aa7cc415
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-03-24 01:01:57 +0530

    Update contact link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         
              <div className="flex gap-3 md:gap-5">
                   <a
-                    href="https://drive.google.com/file/d/1vuPKlvINHxjV9UVpiAVklwPEP2lhDi3Y/view?usp=sharing"
+                    href="https://drive.google.com/file/d/17Xh1LMhkE2c1hNRkEKnS4rfP_cXxxZVp/view?usp=sharing"
                     className="contact-btn group"
                     target="_blank"
                   >
